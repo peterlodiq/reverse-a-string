5f5889fc82339c6f02759e185b6c95ad8b509df4
Péter Lódi <43398810+peterlodiq@users.noreply.github.com>
2018-09-19 08:25:02 +0200

    Uploaded source code
    Uploaded the source code .cs file for the application

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This application writes down the chosen array of letters backwards.");
+            Console.WriteLine("Please enter the characters:");
+            string word = Console.ReadLine();
+            string revword = "";
+            for (int i = word.Length - 1; i > -1; i--)
+            {
+                revword += word[i];
+            }
+            Console.WriteLine("The characters reversed: " + revword);
+        }
+    }
+}
